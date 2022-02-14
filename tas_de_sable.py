aabc114c01d5e53aa089fae81d559b291b734d24
Karen RIZK <karen.rizk@ens.uvsq.fr>
2022-02-14 15:34:59 +0100

    Idebut projet et interface graphique

---
+++ tas_de_sable.py
@@ -0,0 +1,38 @@
+############################################
+#GROUPE LDDMP2
+#CHASSOT Elise
+#REYSSET Hugo
+#ASADUZZAMAN Sihad
+#RIZK Karen
+#https://github.com/22102906/tas_de_sable.py
+############################################
+
+###import des modules#####
+import tkinter as tk 
+
+#####variables globales########
+
+
+
+
+
+racine=tk.Tk()
+racine.title("tas_de_sable")
+racine.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
